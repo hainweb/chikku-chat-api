8f2120a55632c10fc43f6b3ec770c5141761d2a9
hainweb <hainofficialweb@gmail.com>
2024-07-16 15:01:51 +0530

    Update6 index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : ["https://chikku-chat.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+   origin : process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
     credentials : true
 }))
 app.use(express.json())
